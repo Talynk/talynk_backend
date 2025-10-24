bf245621b735f0aa9c81d3c7539a5ba5e67ca413
unknown <nipcts3@gmail.com>
2025-10-24 10:53:02 +0200

    Fix: Modify the routes order in posts to fix the UUID Issues

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -10,14 +10,16 @@ const postController = require('../controllers/postController');
 
 // Post routes (all protected)
 router.post('/', authenticate, ...upload.single('file'), postController.createPost);
+
+// SPECIFIC ROUTES FIRST - before parameterized routes
 router.get('/user', authenticate, postController.getUserPosts);
 router.get('/following', authenticate, postController.getFollowingPosts);
 router.get('/feed', authenticate, postController.getOptimizedFeed);
-router.delete('/:postId', authenticate, postController.deletePost);
-
-// Public post routes
 router.get('/all', postController.getAllPosts); // only approved
 router.get('/search', postController.searchPosts);
+
+// PARAMETERIZED ROUTES LAST - to avoid conflicts
 router.get('/:postId', postController.getPostById);
+router.delete('/:postId', authenticate, postController.deletePost);
 
 module.exports = router;
