fc46ca7a5dd888627366bc2199c265e1d5d8daa9
unknown <nipcts3@gmail.com>
2025-12-28 09:42:48 +0200

    [FIX]: DB COnnection Issue

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,13 +2,23 @@ require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
+  log: process.env.NODE_ENV === 'production' ? ['error', 'warn'] : ['query', 'info', 'warn', 'error'],
   errorFormat: 'pretty',
 });
 
 // Handle graceful shutdown
 process.on('beforeExit', async () => {
   await prisma.$disconnect();
+});
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
 
 module.exports = prisma;
